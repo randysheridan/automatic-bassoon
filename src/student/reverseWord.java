f112d48dc5d0685508ab3f38d293c1a9c9fa86fe
Randy Sadkowski <sadkowsr@sheridancollege.ca>
2023-01-20 13:47:52 -0500

    Added a comment to reverseWord.java

--- src/student/reverseWord.java
+++ src/student/reverseWord.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class reverseWord {
 
     public static void main(String[] args) {
+        
+        // Reversing the user inputted word.
         Scanner userInput = new Scanner(System.in);
         System.out.print("Enter a word: ");
 
